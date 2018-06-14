095c92b1169a383b9a2c579a938abb9f61536a9d
Usman Aziz <usman.aziz@groupdocs.com>
2018-06-15 00:53:53 +0500

    Updated Examples

---
+++ Examples/src/main/java/com/groupdocs/parser/examples/Tools/MediaTypeDetectors.java
@@ -0,0 +1,88 @@
+package com.groupdocs.parser.examples.Tools;
+
+import com.groupdocs.parser.CellsMediaTypeDetector;
+import com.groupdocs.parser.MediaTypeNames;
+import com.groupdocs.parser.NoteMediaTypeDetector;
+import com.groupdocs.parser.examples.Common;
+
+public class MediaTypeDetectors {
+	private final static String FILE_PATH = "sample.xlsx";
+
+	/**
+	 * Detects media type by content.
+	 * 
+	 */
+	public static void detectMediaTypeByContent() {
+		try {
+			// ExStart:detectMediaTypeByContent 
+			// Create a media type detector
+			CellsMediaTypeDetector detector = new CellsMediaTypeDetector();
+			// Detect the media type by the content of the stream
+			String mediaType = detector.detect(new java.io.FileInputStream(Common.mapSourceFilePath(FILE_PATH)));
+			// Print the detected media type (if detected)
+			System.out.println(mediaType == null ? "Can't detect a media type" : mediaType);
+			// ExEnd:detectMediaTypeByContent
+		} catch (Exception exp) {
+			System.out.println("Exception: " + exp.getMessage());
+			exp.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Detects media type by extension of file.
+	 * 
+	 */
+	public static void detectMediaTypeByExtension() {
+		try {
+			// ExStart:detectMediaTypeByExtension
+			// Create a media type detector
+			CellsMediaTypeDetector detector = new CellsMediaTypeDetector();
+			// Detect the media type by the content of the stream
+			String mediaType = detector.detect(Common.mapSourceFilePath(FILE_PATH));
+			// Print the detected media type (if detected)
+			System.out.println(mediaType == null ? "Can't detect a media type" : mediaType);
+			// ExEnd:detectMediaTypeByExtension
+		} catch (Exception exp) {
+			System.out.println("Exception: " + exp.getMessage());
+			exp.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Detects whether the media type is supported by the detector.
+	 * 
+	 */
+	public static void IsMediaTypeSupportedByDetector() {
+		try {
+			// ExStart:IsMediaTypeSupportedByDetector
+			// Create a media type detector
+			CellsMediaTypeDetector detector = new CellsMediaTypeDetector();
+			// Write true if the detector supports Excel files
+			System.out.println(detector.supports(MediaTypeNames.Application.EXCEL));
+			// ExEnd:IsMediaTypeSupportedByDetector
+		} catch (Exception exp) {
+			System.out.println("Exception: " + exp.getMessage());
+			exp.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Detects media type of OneNote section
+	 * 
+	 */
+	public static void detectMediaTypeOfOneNoteSection() {
+		try {
+			// ExStart:detectMediaTypeOfOneNoteSection
+			// Create a media type detector
+			NoteMediaTypeDetector detector = new NoteMediaTypeDetector();
+			// Detect the media type by the content of the stream
+			String mediaType = detector.detect(Common.mapSourceFilePath(FILE_PATH));
+			// Print the detected media type (if detected)
+			System.out.println(mediaType == null ? "Can't detect a media type" : mediaType);
+			// ExEnd:detectMediaTypeOfOneNoteSection
+		} catch (Exception exp) {
+			System.out.println("Exception: " + exp.getMessage());
+			exp.printStackTrace();
+		}
+	}
+}
